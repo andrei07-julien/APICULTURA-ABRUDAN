f0b891f0dedae198a7363f3f12c05cbae1808c07
Andrei Abrudan <andreiabrudan04@gmail..com>
2024-10-31 22:20:41 +0200

    update map

--- seller.js
+++ seller.js
@@ -1,27 +1,27 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var userLocation = [position.coords.latitude, position.coords.longitude];
-        
-        var map = L.map('map').setView(userLocation, 13);
+navigator.geolocation.getCurrentPosition(function(position) {
+    var userLocation = [position.coords.latitude, position.coords.longitude];
+    var sellerLocation = [47.065948, 22.851891]; // Replace with actual seller coordinates
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+    var map = L.map('map').setView(userLocation, 13);
 
-        L.marker(userLocation).addTo(map)
-            .bindPopup('Locația utilizatorului')
-            .openPopup();
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-        
-        var sellerLocation = [40.7128, -74.0060]; // replace with actual seller coordinates
-        L.Routing.control({
-             waypoints: [
-                L.latLng(userLocation),
-                L.latLng(sellerLocation)
-             ],
-             routeWhileDragging: true
-         }).addTo(map);
-    });
-} else {
-    alert("Geolocation is not supported by this browser.");
-}
+    L.marker(userLocation).addTo(map)
+        .bindPopup('Locația utilizatorului')
+        .openPopup();
+
+    // Add seller marker
+    L.marker(sellerLocation).addTo(map)
+        .bindPopup('Locația vânzătorului')
+        .openPopup();
+
+    L.Routing.control({
+        waypoints: [
+            L.latLng(userLocation),
+            L.latLng(sellerLocation)
+        ],
+        routeWhileDragging: true
+    }).addTo(map);
+});
